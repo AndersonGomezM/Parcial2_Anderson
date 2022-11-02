bdddbce687993261cec6b85ec40c4bd49462d5ec
Anderson <Andersongomez9870@gmail.com>
2022-11-02 19:35:20 -0400

    Programando AppModule, agregando moshi.

--- app/src/main/java/edu/ucne/parcial2_anderson/di/AppModule.kt
+++ app/src/main/java/edu/ucne/parcial2_anderson/di/AppModule.kt
@@ -1,19 +1,36 @@
 package edu.ucne.parcial2_anderson.di
 
-import android.content.Context
-import androidx.room.Room
 import javax.inject.Singleton
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import edu.ucne.parcial2_anderson.data.remote.VerboApi
 
-class AppModule {
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
 
+    @Singleton
+    @Provides
+    fun provideVerboApi(moshi: Moshi): VerboApi {
+        val Base_Url = "https://private-a127e-verbos.apiary-mock.com/verbos"
+        return Retrofit.Builder()
+            .baseUrl(Base_Url)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(VerboApi::class.java)
+    }
 }
